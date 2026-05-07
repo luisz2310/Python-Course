8eb66ebcc43ec28f4feaa2a8dfa485ce67c75f23
luisz2310 <luisz2310@hotmail.com>
2026-05-07 18:54:41 -0500

    excercises

--- Cheat sheets/2 functions (def)/7 exercises lvl2 part 1.py
+++ Cheat sheets/2 functions (def)/7 exercises lvl2 part 1.py
@@ -38,3 +38,23 @@ def blackjack(*arg):
 print(blackjack(5,6,7))
 print(blackjack(9,9,9))
 print(blackjack(9,9,11))
+print("-------------------")
+
+# SUMMER OF '69: Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 9 (every 6 will be followed by at least one 9). Return 0 for no numbers.
+#     summer_69([1, 3, 5]) --> 9
+#     summer_69([4, 5, 6, 7, 8, 9]) --> 9
+#     summer_69([2, 1, 6, 9, 11]) --> 14
+def summer_69(args):
+    a = 0
+    flag = False
+    for arg in args:
+        if arg == 6:
+            flag = True
+        if not flag:
+            a += arg
+        if arg == 9:
+            flag = False
+    return a
+print(summer_69([1, 3, 5]))
+print(summer_69([4, 5, 6, 7, 8, 9]))
+print(summer_69([2, 1, 6, 9, 11]))
